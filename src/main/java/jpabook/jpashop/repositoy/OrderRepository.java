7b6d2ce815574faef9d77503f966353525f1b693
lkimilhol <lkimilhol@gmail.com>
2021-05-09 22:24:07 +0900

    주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repositoy/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repositoy/OrderRepository.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.repositoy;
 
+import java.util.List;
+
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 
@@ -10,17 +12,27 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
-    private final EntityManager em;
+	private final EntityManager em;
 
-    public void save(Order order) {
-        em.persist(order);
-    }
+	public void save(Order order) {
+		em.persist(order);
+	}
 
-    public Order findOne(Long id) {
-        return em.find(Order.class, id);
-    }
+	public Order findOne(Long id) {
+		return em.find(Order.class, id);
+	}
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+	public List<Order> findAll(OrderSearch orderSearch) {
+		// TODO QueryDsl로 해결 해야 한다.
+		// return em.createQuery("select o from Order o join o.member m" +
+		// 	"where o.status = :status" +
+		// 	"and m.name like :name", Order.class)
+		// 	.setParameter("status", orderSearch.getOrderStatus())
+		// 	.setParameter("name", orderSearch.getMemberName())
+		// 	.setMaxResults(100)
+		// 	.getResultList()
+		// 	;
+		return null;
+	}
+
 }
